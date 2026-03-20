c2251eb9ec603915eb47759e816695edf42fc09e
KHSH Edirisinghe <siharaedirisinghe03@gmail.com>
2026-03-20 23:55:51 +0530

    Initial project setup

---
+++ frontend/bookgenie/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-500">
+      <h1 className="text-4xl text-white">Tailwind CSS 3.4.17 is working!</h1>
+    </div>
+  )
+}
+
+export default App
