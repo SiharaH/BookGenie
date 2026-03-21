263352a36839bece4f664b6bf4fba9292530ee92
KHSH Edirisinghe <siharaedirisinghe03@gmail.com>
2026-03-21 23:20:44 +0530

    Create project folder structure

--- frontend/bookgenie/src/pages/LandingPage.jsx
+++ frontend/bookgenie/src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const App = () => {
+const LandingPage = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const App = () => {
   )
 }
 
-export default App
+export default LandingPage
--- frontend/bookgenie/src/pages/ProfilePage.jsx
+++ frontend/bookgenie/src/pages/ProfilePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const App = () => {
+const ProfilePage = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const App = () => {
   )
 }
 
-export default App
+export default ProfilePage
